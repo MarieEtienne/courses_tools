e2ee3e2c69906904548e81402049e370be7c6082
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2021-02-24 15:36:22 +0100

    Change install.R

--- R/install.R
+++ R/install.R
@@ -13,58 +13,62 @@ install.packages('tidyverse')
 # graphic -----------------------------------------------------------------
 install.packages('plotly')
 install.packages('ggpubr')
+install.packages("gifski")
+install.packages("magick")
+install.packages("leaflet")
+
+
+
 # ## sudo apt-get install cargo 
 # install.packages('gifski') 
 # ## sudo add-apt-repository -y ppa:cran/imagemagick
 # ## sudo apt-get update
 # ## sudo apt-get install -y libmagick++-dev
-# install.package('magick')   
-# install.packages('gganimate')
-# install.packages('GGally')
-# 
-# graph/reseau ---------------------------------------------------------------
-install.packages('igraph')
+# install.packages('magick')   
+install.packages('gganimate')
+install.packages('GGally')
 
-install.packages('ggraph')
-install.packages('tidygraph')
+# linear model ------------------------------------------------------------
 
-install.packages('network')
-install.packages('sna')
-install.packages('animation')
+install.packages("ggfortify") ## nice diagnostic plot for lm glm and co
+install.packages('car') # functionAnova for type III
+install.packages("emmeans") # lsmeans
 
 
-# # color -------------------------------------------------------------------
-# install.packages('wesanderson')
-# 
-# # maps --------------------------------------------------------------------
-# install.packages('sf')
-# install.packages('ggmap')
-# install.packages('rnaturalearth')
-# install.packages('rnaturalearthdata')
-# install.packages('rgeos')
-# 
-# # reporting ---------------------------------------------------------------
-# install.packages('rmarkdown')
-# install.packages('kableExtra')
-# install.packages('RefManageR')
+# color -------------------------------------------------------------------
+install.packages('wesanderson')
 
-# 
-# # presentation ------------------------------------------------------------
-# 
-# 
-# 
-# ##########################################################
-# #### GitHub package
-# ##########################################################
-# 
-# 
+# maps --------------------------------------------------------------------
+
+
+
+# sudo apt install libpq-dev
+# sudo apt install gdal-bin
+# sudo apt-get install -y libgdal-dev
+# sudo apt install libudunits2-dev 
+install.packages('sf')
+install.packages('maps')
+install.packages('ggmap')
+install.packages('rnaturalearth')
+install.packages('rnaturalearthdata')
+install.packages('rgeos')
+remotes::install_github("ropensci/osmdata") # openstreet map data
+
+# reporting ---------------------------------------------------------------
+install.packages('rmarkdown')
+install.packages('kableExtra')
+remotes::install_github("ropensci/RefManageR")
+install.packages('flexdashboard') # to produce nice dashboard
+
+
+
+
 # # data --------------------------------------------------------------------
-# remotes::install_github("allisonhorst/palmerpenguins")
-# 
+remotes::install_github("allisonhorst/palmerpenguins")
+remotes::install_github("MarieEtienne/coursesdata")
+
 # # presentation ------------------------------------------------------------
-# remotes::install_github('yihui/xaringan')
-# remotes::install_github("EvaMaeRey/flipbookr")
-# 
-# # map ---------------------------------------------------------------------
-# remotes::install_github("ropensci/osmdata")
-# # 
+remotes::install_github('yihui/xaringan')
+remotes::install_github("EvaMaeRey/flipbookr")
+remotes::install_github("gadenbuie/xaringanExtra")
+
