d7a472a1f7c0a2e750800dc05632a7f07215d0a4
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2021-03-08 14:07:11 +0100

    Add common package list

--- R/load_packages.R
+++ R/load_packages.R
@@ -1,18 +1,26 @@
 ##########################################################
 #### cran packages
 ##########################################################
-packages_to_be_installed <- c('../sources/package_list.txt', 'common_package_list.txt')
+packages_to_be_installed <- c(paste0(main_dir,'resources/project_package_list.txt'), 
+                              paste0(main_dir, '/courses_tools/resources/common_package_list.txt'))
 
+print(packages_to_be_installed)
 for ( l in packages_to_be_installed){
   if(file.exists(l)){
-    package_list <- as.character(read.table(l))
-    
-    for( p_ in package_list){
-      if(!require(p_,character.only = TRUE)){
-        cat(paste0('Package ', p_, ' mentionned in ', l, ' is missing. Stop rendering'))
+    package_list <- read.table(l, header = TRUE, sep = ',')
+    if(nrow(package_list)>0){
+      for( i in 1:nrow(package_list)){
+        if(!require(package_list$name[i],character.only = TRUE)){
+          cat(paste0('Package ', package_list$name[i], ' mentionned in ', l, ' is missing. Now installing it'))
+          if(package_list$repo == 'cran'){
+            install.packages(package_list$name[i])
+          } else {
+            remotes::install_github(paste0(repo,'/',name))
+          }
+          library(package_list$name[i],character.only = TRUE)
+        }
       }
     }
   }
 }
-
 
