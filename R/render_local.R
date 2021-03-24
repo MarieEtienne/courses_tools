12c0128dcadb3234c98af31eea424bc18037f939
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2021-03-24 17:58:15 +0100

    add render_local.R

--- R/render_local.R
+++ R/render_local.R
@@ -1,21 +1,29 @@
-#system('rm -rf render')
-system('mkdir -p render')
+
+rmarkdown::render_site(encoding = 'UTF-8')
+
+
+system('mkdir render/courses_tools') 
+system('mkdir render/resources') 
+system( glue::glue( 'cp -rf courses_tools/resources render/courses_tools/.'))
+system( glue::glue( 'cp -rf resources/figs render/resources/.'))
 
 source(file.path('courses_tools','R', 'load_packages.R'))
 
-directory_list <- list.files('sources', full.names = TRUE)
-for( directory in directory_list){
-  
-  filenames <- list.files(directory, pattern = '.Rmd',
-                          full.names = TRUE)
-  system(glue::glue( 'cp resources/mpe_pres.css {directory}/.'))
-  
-  for(f_ in filenames){
-    system(glue::glue( 'rm -rf {stringr::str_remove(f_, ".Rmd")}_cache'))
-    system(glue::glue( 'rm -rf {stringr::str_remove(f_, ".Rmd")}_files'))
-  }
-  
-  rmarkdown::render(file.path(directory, 'index.Rmd'))
-  
-  system( glue::glue( 'cp -rf  {directory} render/ '))
+
+
+filenames <- file.path('_presentation', 'modelelineaire.Rmd')
+
+system(glue::glue( 'cp resources/mpe_pres.css _presentation/.'))
+system(glue::glue( 'cp resources/*.bib _presentation/.'))
+
+
+for(f_ in filenames){
+  rmarkdown::render(f_)
 }
+
+
+system( glue::glue( 'cp -rf _presentation render/. '))
+
+
+
+
