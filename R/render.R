7b739b950aa546a6378c9f4a1df19c1a6f2423fc
Marie-Pierre Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2023-03-01 18:50:34 +0100

    copy resources/figs if it exists only

--- R/render.R
+++ R/render.R
@@ -5,7 +5,8 @@ system('mkdir -p render')
 system('mkdir render/courses_tools') 
 system('mkdir render/resources') 
 system( glue::glue( 'cp -rf courses_tools/resources render/courses_tools/.'))
-system( glue::glue( 'cp -rf resources/figs render/resources/.'))
+if(file.exists('resources/figs'))
+  system( glue::glue( 'cp -rf resources/figs render/resources/.'))
 
 source(file.path('courses_tools','R', 'load_packages.R'))
 
